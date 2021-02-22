86e85c4aa7ad24890d2b5346494b9e0a2fa7f6fa
Norewal <63234816+Norewal@users.noreply.github.com>
2021-02-22 21:30:11 +0100

    third commit

--- NodeFarm/index.js
+++ NodeFarm/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 
@@ -10,6 +11,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); 
 const dataObj = JSON.parse(data);
+
+//console.log(slugify('Fresh Avocados', { lower: true }));
+const slugs = dataObj.map(el => slugify(el.name, { lower: true }));
+//console.log(slugs);
+
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
@@ -42,4 +48,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
-} )
+});
